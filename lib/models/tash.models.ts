4171aae688e3be4756aaff10668d6061bee71305
GodwinAdu <gyamfiadu01@gmail.com>
2025-09-04 13:22:19 +0000

    update the dashboard page for role access

--- lib/models/tash.models.ts
+++ lib/models/tash.models.ts
@@ -1,10 +1,19 @@
 import { model, Schema, models, Model } from 'mongoose';
 
+interface ITrash {
+    originalCollection: string;
+    document: Record<string, unknown>;
+    message?: string;
+    deletedBy?: Schema.Types.ObjectId;
+    deletedAt?: Date;
+    autoDelete?: boolean;
+}
+
 const trashSchema: Schema<ITrash> = new Schema({
     originalCollection: { type: String, required: true },
     document: { type: Schema.Types.Mixed, required: true },
     message: { type: String },
-    deletedBy: { type: Schema.Types.ObjectId, ref: 'Staff' },
+    deletedBy: { type: Schema.Types.ObjectId, ref: 'User' },
     deletedAt: { type: Date, default: Date.now },
     autoDelete: { type: Boolean, default: false }, // Defaults will be set dynamically
 }, {
